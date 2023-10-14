1a438a2d80fd9f7a341da23a6a5df7d132ddfe42
yutaro <1212yutaro@gmail.com>
2023-10-14 18:18:16 +0900

    1014-002

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
@@ -11,6 +12,7 @@ class ResetPasswordController extends Controller
     use ResetsPasswords;
 
     protected $redirectTo = '/mypage';
+
 
     public function showResetForm(Request $request, $token)
     {
